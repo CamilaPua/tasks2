ad4c6ce7e0ca19b72b1283ea0e1f58d9ddd52e44
Camila <mariacamilaarciniegaspua@gmail.com>
2023-10-06 15:20:20 -0500

    Add views for add_task and index

---
+++ app/blueprints/add_task_route.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, request, redirect
+from app.models import Task
+from app import forms
+
+# blueprint configuration
+add_task_bp = Blueprint(
+    'add_task_bp', __name__,
+)
+
+@add_task_bp.route('/add-task', methods=['GET', 'POST'])
+def add_task():
+    tasks_form = forms.Task(request.form)
+    if request.method == 'POST':
+        Task.add_task(tasks_form.description.data)
+        return redirect('/')
+    return render_template('add_task.html', tasks_form=tasks_form)
---
+++ app/blueprints/index_route.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from app.models import Task
+
+# blueprint configuration
+index_bp = Blueprint(
+    'index_bp', __name__,
+)
+
+@index_bp.route('/')
+def index():
+    tasks = Task.get_tasks()
+    return render_template('index.html', tasks=tasks)
