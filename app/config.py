01c566ae1836309305efdc546a5cbb7ed720bba7
Camila Pua <camilapua7@gmail.com>
2023-10-17 21:37:03 -0500

    fix config: super key

--- app/config.py
+++ app/config.py
@@ -1,13 +1,7 @@
-from os import environ, path
-from dotenv import load_dotenv
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, ".env"))
-
 # general config
 FLASK_APP = 'app'
 FLASK_DEBUG = True
-SECRET_KEY = environ.get('SECRET_KEY')
+SECRET_KEY = 'a super key'
 
 # database
 SQLALCHEMY_DATABASE_URI = 'sqlite:///tasks.db'
